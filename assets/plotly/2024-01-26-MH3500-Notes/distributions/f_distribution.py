66b804de905ab95141ccc11e22ad6e0259669be0
pufanyi <pufanyi@gmail.com>
2024-02-05 07:20:11 +0000

    Deploying to gh-pages from @ pufanyi/pufanyi.github.io@8dd514f5ba79ca562a1324a8eabda7bcd1a205aa 🚀

--- assets/plotly/2024-01-26-MH3500-Notes/distributions/f_distribution.py
+++ assets/plotly/2024-01-26-MH3500-Notes/distributions/f_distribution.py
@@ -3,31 +3,33 @@ import numpy as np
 import math
 
 
-def F_distribution(m, n):
-    return 
+def F_distribution(w, m, n):
+    if w <= 0:
+        return 0
+    return math.gamma((m + n) / 2) / (math.gamma(m / 2) * math.gamma(n / 2)) * math.pow(m / n, m / 2) * math.pow(w, m / 2 - 1) * math.pow(1 + m / n * w, -(m + n) / 2)
 
 
 fig = go.Figure()
 
-alphas = np.arange(0.5, 5, 0.1)
+Ms = [1, 2, 3, 4, 5, 10, 20, 50, 100]
 
-for step in alphas:
+for step in Ms:
     x = np.arange(0, 10, 0.01)
-    y = [gamma_distribution(i, step, 1) for i in x]
+    y = [F_distribution(i, step, 10) for i in x]
     fig.add_trace(
         go.Scatter(
             visible=False,
             # line=dict(color="#00CED1", width=6),
-            name="α = " + str(step),
+            name=f"m = {step}",
             x=x,
             y=y,
         )
     )
 
 # Make 10th trace visible
-fig.data[10].visible = True
+fig.data[3].visible = True
 fig.update_layout(
-    title="Gamma Distribution (α = 1.5, β = 1)",
+    title="F(4, 10) Distribution",
 )
 
 # Create and add slider
@@ -37,7 +39,7 @@ for i in range(len(fig.data)):
         method="update",
         args=[
             {"visible": [False] * len(fig.data)},
-            {"title": "α = {:.1f}".format(float(alphas[i]))},
+            {"title": f"F({i + 1}, 10) Distribution"},
         ],  # layout attribute
     )
     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
@@ -45,46 +47,46 @@ for i in range(len(fig.data)):
 
 sliders = [
     dict(
-        active=10,
+        active=3,
         steps=[
             dict(
-                label="α = {:.1f}".format(alpha),
+                label=f"m = {m}",
                 method="update",
                 args=[
-                    {"visible": [alpha == step for step in alphas]},
-                    {"title": "Gamma Distribution (α = {:.1f}, β = 1)".format(alpha)},
+                    {"visible": [m == step for step in Ms]},
+                    {"title": f"F({m + 1}, 10) Distribution"},
                 ],
             )
-            for alpha in alphas
+            for m in Ms
         ],
     )
 ]
 
 fig.update_layout(sliders=sliders)
 
-fig.write_html("gamma_distribution_change_alpha.html", auto_open=True)
+fig.write_html("F_distribution_change_M.html", auto_open=True)
 
 fig = go.Figure()
 
-betas = np.arange(0.5, 5, 0.1)
+Ns = [1, 2, 3, 4, 5, 10, 20, 50, 100]
 
-for step in betas:
+for step in Ns:
     x = np.arange(0, 10, 0.01)
-    y = [gamma_distribution(i, 2, step) for i in x]
+    y = [F_distribution(i, 10, step) for i in x]
     fig.add_trace(
         go.Scatter(
             visible=False,
             # line=dict(color="#00CED1", width=6),
-            name="β = " + str(step),
+            name = f"n = {step}",
             x=x,
             y=y,
         )
     )
 
 # Make 10th trace visible
-fig.data[10].visible = True
+fig.data[3].visible = True
 fig.update_layout(
-    title="Gamma Distribution (α = 2, β = 1)",
+    title = "F(10, 4) Distribution",
 )
 
 # Create and add slider
@@ -94,7 +96,7 @@ for i in range(len(fig.data)):
         method="update",
         args=[
             {"visible": [False] * len(fig.data)},
-            {"title": "β = {:.1f}".format(float(betas[i]))},
+            {"title": f"F(10, {Ns[i]}) Distribution"},
         ],  # layout attribute
     )
     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
@@ -102,21 +104,21 @@ for i in range(len(fig.data)):
 
 sliders = [
     dict(
-        active=10,
+        active=3,
         steps=[
             dict(
-                label="β = {:.1f}".format(beta),
+                label=f"n = {n}",
                 method="update",
                 args=[
-                    {"visible": [beta == step for step in alphas]},
-                    {"title": "Gamma Distribution (α = 2, β = {:.1f})".format(beta)},
+                    {"visible": [n == step for step in Ns]},
+                    {"title": f"F(10, {n}) Distribution"},
                 ],
             )
-            for beta in betas
+            for n in Ns
         ],
     )
 ]
 
 fig.update_layout(sliders=sliders)
 
-fig.write_html("gamma_distribution_change_beta.html", auto_open=True)
+fig.write_html("F_distribution_change_N.html", auto_open=True)
